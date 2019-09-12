3496b81a78dea12a371c01e892fd99f5e04ae2db
Andrey Helldar <helldar@ai-rus.com>
2019-09-12 17:58:40 +0300

    First Initial

---
+++ src/ServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Helldar\LaravelApp;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    protected $defer = false;
+
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
